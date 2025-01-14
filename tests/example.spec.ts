47aede256836d5a0646c42ed9c4b80fa97e22f7c
Noam Gaash <noam.gaash@applitools.com>
2025-01-14 17:12:22 +0200

    clean up things

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,6 @@
-import { test, expect } from '@applitools/eyes-playwright/fixture';
+import { test } from '@applitools/eyes-playwright/fixture';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('example test', async ({ page, eyes }) => {
+  await page.goto('https://example.com');
+  await eyes.check('Main Page of Example.com');
 });
